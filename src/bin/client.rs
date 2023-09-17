aeafccc67d50de132cdb84b882a4832ffca81dc1
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2023-09-17 20:14:07 +0200

    Faster keyboard input on non macos

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,18 +1,67 @@
 use std::num::NonZeroU32;
 
 use glutin::surface::GlSurface;
-use infinirust::game::Key;
-use winit::{event::{Event, WindowEvent, ElementState, VirtualKeyCode, DeviceEvent}, window::CursorGrabMode};
+use infinirust::game::{Game, Key};
+use winit::{
+    event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    window::CursorGrabMode,
+};
 
 fn main() {
     let (event_loop, window, surface, gl_context) = infinirust::window::create_window();
 
-    let mut game = infinirust::game::Game::new();
+    let mut game = Game::new();
 
     let mut now = std::time::SystemTime::now();
 
-    event_loop.run(move |event, window_target, control_flow| {
+    
+
+    event_loop.run(move |event, _window_target, control_flow| {
         control_flow.set_poll();
+        println!("{:?}", event);
+
+        let mut handle_keyboard = |input : KeyboardInput| {
+            let pressed = match input.state {
+                ElementState::Pressed => true,
+                ElementState::Released => false,
+            };
+            match input.virtual_keycode {
+                Some(VirtualKeyCode::A) => {
+                    game.keyboard_input(Key::Left, pressed);
+                }
+                Some(VirtualKeyCode::S) => {
+                    game.keyboard_input(Key::Backward, pressed);
+                }
+                Some(VirtualKeyCode::D) => {
+                    game.keyboard_input(Key::Right, pressed);
+                }
+                Some(VirtualKeyCode::W) => {
+                    game.keyboard_input(Key::Forward, pressed);
+                }
+                Some(VirtualKeyCode::LShift) => {
+                    game.keyboard_input(Key::Down, pressed);
+                }
+                Some(VirtualKeyCode::Space) => {
+                    game.keyboard_input(Key::Up, pressed);
+                }
+                Some(VirtualKeyCode::Escape) => {
+                    control_flow.set_exit();
+                }
+                Some(VirtualKeyCode::G) => {
+                    window
+                        .set_cursor_grab(CursorGrabMode::Confined)
+                        .or_else(|_e| window.set_cursor_grab(CursorGrabMode::Locked))
+                        .unwrap();
+                    window.set_cursor_visible(false);
+                }
+                Some(VirtualKeyCode::R) => {
+                    window.set_cursor_grab(CursorGrabMode::None).unwrap();
+                    window.set_cursor_visible(true);
+                }
+                _ => {}
+            }
+        };
+
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::Resized(size) => {
@@ -38,51 +87,13 @@ fn main() {
                 WindowEvent::CloseRequested => {
                     control_flow.set_exit();
                 }
+                #[cfg(target_os = "macos")]
                 WindowEvent::KeyboardInput {
                     device_id: _,
                     input,
                     is_synthetic: _,
                 } => {
-                    println!("{:?}", input);
-                    let pressed = match input.state {
-                        ElementState::Pressed => true,
-                        ElementState::Released => false,
-                    };
-                    match input.virtual_keycode {
-                        Some(VirtualKeyCode::A) => {
-                            game.keyboard_input(Key::Left, pressed);
-                        }
-                        Some(VirtualKeyCode::S) => {
-                            game.keyboard_input(Key::Backward, pressed);
-                        }
-                        Some(VirtualKeyCode::D) => {
-                            game.keyboard_input(Key::Right, pressed);
-                        }
-                        Some(VirtualKeyCode::W) => {
-                            game.keyboard_input(Key::Forward, pressed);
-                        }
-                        Some(VirtualKeyCode::LShift) => {
-                            game.keyboard_input(Key::Down, pressed);
-                        }
-                        Some(VirtualKeyCode::Space) => {
-                            game.keyboard_input(Key::Up, pressed);
-                        }
-                        Some(VirtualKeyCode::Escape) => {
-                            control_flow.set_exit();
-                        }
-                        Some(VirtualKeyCode::G) => {
-                            window
-                                .set_cursor_grab(CursorGrabMode::Confined)
-                                .or_else(|_e| window.set_cursor_grab(CursorGrabMode::Locked))
-                                .unwrap();
-                            window.set_cursor_visible(false);
-                        }
-                        Some(VirtualKeyCode::R) => {
-                            window.set_cursor_grab(CursorGrabMode::None).unwrap();
-                            window.set_cursor_visible(true);
-                        }
-                        _ => {}
-                    }
+                    handle_keyboard(input);
                 }
                 _ => (),
             },
@@ -101,6 +112,10 @@ fn main() {
             } => match event {
                 DeviceEvent::MouseMotion { delta } => {
                     game.mouse_input(delta);
+                }
+                #[cfg(not(target_os="macos"))]
+                DeviceEvent::Key(input) => {
+                    handle_keyboard(input);
                 }
                 _ => (),
             },
