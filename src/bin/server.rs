9037c37f090f47d52c39fc2a170bdb3475408038
Benjamin Lieser <benjamin.lieser@stud.uni-goettingen.de>
2023-10-08 23:29:08 +0200

    server might work

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,10 @@
 use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{
+    tcp::{OwnedReadHalf, OwnedWriteHalf},
+    TcpListener, TcpStream,
+};
 
-use infinirust::game::ServerWorld;
+use infinirust::server::{world::ServerWorld, Command};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 fn main() -> std::io::Result<()> {
@@ -13,26 +16,68 @@ fn main() -> std::io::Result<()> {
     rt.block_on(async {
         let listener = TcpListener::bind("127.0.0.1:8042").await.unwrap();
         //common shared state
-        let world = Arc::new(Mutex::new(ServerWorld::new(42)));
+        let world = ServerWorld::new(42);
+
+        let (command_tx, command_rx) = tokio::sync::mpsc::channel(10);
+
+        let (write_package_tx, _) = tokio::sync::broadcast::channel(10);
+
+
+        let write_package_tx2 = write_package_tx.clone();        
+        std::thread::spawn(|| infinirust::server::manage_chunk_data(command_rx, write_package_tx2, world));
+
         // accept connections and process them in a new thread
-        for (stream, _) in listener.accept().await {
-            let world = world.clone();
-            tokio::task::spawn(async {
-                handle_connection(stream, world).await;
-            });
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            let (read, write) = stream.into_split();
+
+            tokio::task::spawn(write_packages(write, write_package_tx.subscribe()));
+            tokio::task::spawn(read_packages(read, command_tx.clone()));
         }
     });
 
     Ok(())
 }
-async fn handle_connection(mut stream: TcpStream, world: Arc<Mutex<ServerWorld>>) {
-    let mut pos = [0i32; 3];
+
+async fn write_packages(
+    mut stream: OwnedWriteHalf,
+    mut input: tokio::sync::broadcast::Receiver<Arc<[u8]>>,
+) {
     loop {
-        stream
-            .read_exact(infinirust::misc::as_bytes_mut(&mut pos))
-            .await
-            .unwrap();
-        let mut world = world.lock().unwrap();
-        world.write_chunk(&pos, &mut stream).await;
+        let package = input.recv().await.unwrap();
+        //TODO: logic to discard packages which are not relevant to the loaded chunks of the client
+        stream.write_all(&package).await.unwrap();
+    }
+}
+
+async fn read_packages(mut stream: OwnedReadHalf, output: tokio::sync::mpsc::Sender<Command>) {
+    loop {
+        let mut package_type = [0u8; 2];
+        stream.read_exact(&mut package_type).await.unwrap();
+        eprintln!("{:?}", package_type);
+        match u16::from_le_bytes(package_type) {
+            // Request chunk data
+            0x0A => {
+                let mut pos = [0i32; 3];
+                stream
+                    .read_exact(infinirust::misc::as_bytes_mut(&mut pos))
+                    .await
+                    .unwrap();
+                let command = Command::ChunkData(pos);
+                output.send(command).await.unwrap();
+            }
+            // Request chunk data
+            0x0B => {
+                // Send block update
+                let mut pos = [0i32; 3];
+                stream
+                    .read_exact(infinirust::misc::as_bytes_mut(&mut pos))
+                    .await
+                    .unwrap();
+            }
+            _ => {
+                panic!("Invalid package type")
+            }
+        }
     }
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,9 +1,10 @@
 pub mod world;
+#[derive(Debug)]
 pub enum BlockUpdateMode {
     Destroy,
     Place
 }
-
+#[derive(Debug)]
 pub enum Command {
     ChunkData([i32;3]),
     BlockUpdate([i32;3], BlockUpdateMode, u8)
