1628c8004e66b531b7e74624302121d4ca91650f
Benjamin Lieser <benjamin.lieser@stud.uni-goettingen.de>
2025-06-24 10:33:01 +0200

    udated winit stuff (major refactor)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,145 @@
 use std::{io::Write, mem::ManuallyDrop, num::NonZeroU32};
 
-use glutin::surface::GlSurface;
+use glutin::surface::{GlSurface, Surface, WindowSurface};
 use infinirust::{
     game::{Game, Key},
     misc::{login, start_server},
     mygl::GLToken,
 };
-use winit::event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::{
+    application::ApplicationHandler,
+    event::{ElementState, WindowEvent},
+    event_loop::ControlFlow,
+    keyboard::KeyCode,
+};
+
+struct App {
+    game: Game,
+    time: std::time::SystemTime,
+    glt: GLToken,
+    surface: Surface<WindowSurface>,
+    gl_context: glutin::context::PossiblyCurrentContext,
+    window: winit::window::Window,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        event_loop.set_control_flow(ControlFlow::Poll);
+    }
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::Resized(size) => {
+                if size.width != 0 && size.height != 0 {
+                    self.surface.resize(
+                        &self.gl_context,
+                        NonZeroU32::new(size.width).unwrap(),
+                        NonZeroU32::new(size.height).unwrap(),
+                    );
+                    self.game.resize(self.glt, size);
+                }
+            }
+            WindowEvent::Focused(is_focused) => {
+                if is_focused {
+                    self.window.set_cursor_visible(false);
+                    //window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
+                } else {
+                    //window.set_fullscreen(None);
+                    //window.set_cursor_grab(CursorGrabMode::None).unwrap();
+                    self.window.set_cursor_visible(true);
+                }
+            }
+            WindowEvent::KeyboardInput {
+                device_id: _,
+                event,
+                is_synthetic: _,
+            } => {
+                let pressed = match event.state {
+                    ElementState::Pressed => true,
+                    ElementState::Released => false,
+                };
+                if let winit::keyboard::PhysicalKey::Code(code) = event.physical_key {
+                    match code {
+                        KeyCode::KeyA => {
+                            self.game.keyboard_input(Key::Left, pressed);
+                        }
+                        KeyCode::KeyS => {
+                            self.game.keyboard_input(Key::Backward, pressed);
+                        }
+                        KeyCode::KeyD => {
+                            self.game.keyboard_input(Key::Right, pressed);
+                        }
+                        KeyCode::KeyW => {
+                            self.game.keyboard_input(Key::Forward, pressed);
+                        }
+                        KeyCode::ShiftLeft => {
+                            self.game.keyboard_input(Key::Down, pressed);
+                        }
+                        KeyCode::Space => {
+                            self.game.keyboard_input(Key::Up, pressed);
+                        }
+                        KeyCode::Escape => {
+                            event_loop.exit();
+                        }
+                        KeyCode::KeyF => {
+                            self.window
+                                .set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
+                            //window.set_cursor_visible(false);
+                        }
+                        KeyCode::KeyG => {
+                            //window.set_cursor_grab(CursorGrabMode::None).unwrap();
+                            //window.set_cursor_visible(true);
+                            self.window.set_fullscreen(None)
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            WindowEvent::MouseInput {
+                device_id: _,
+                state,
+                button,
+            } => {}
+            WindowEvent::RedrawRequested => {}
+            _ => {}
+        }
+    }
+
+    fn about_to_wait(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
+        let current_time = std::time::SystemTime::now();
+        let delta_t = current_time.duration_since(self.time).unwrap();
+        self.time = current_time;
+
+        println!("{}", delta_t.as_millis());
+
+        self.game.draw(self.glt, delta_t.as_secs_f32());
+
+        //game.print_dist();
+
+        self.surface.swap_buffers(&self.gl_context).unwrap();
+    }
+
+    fn device_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        device_id: winit::event::DeviceId,
+        event: winit::event::DeviceEvent,
+    ) {
+        match event {
+            winit::event::DeviceEvent::MouseMotion { delta } => {
+                self.game.mouse_input((delta.0, delta.1));
+            }
+
+            _ => {}
+        }
+    }
+
+    fn exiting(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {}
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -23,125 +156,34 @@ fn main() {
     // It is save to create the GLToken in the main thread
     let glt = unsafe { GLToken::new() };
 
-    let mut game = ManuallyDrop::new(Game::new(glt, window.inner_size(), server_tcp, uid as usize, args[2].clone()));
+    let game = Game::new(
+        glt,
+        window.inner_size(),
+        server_tcp,
+        uid as usize,
+        args[2].clone(),
+    );
 
-    let mut now = std::time::SystemTime::now();
+    let now = std::time::SystemTime::now();
 
-    event_loop.run(move |event, _window_target, control_flow| {
-        control_flow.set_poll();
-        //println!("{:?}", event);
+    let mut app = App {
+        game: game,
+        time: now,
+        glt,
+        surface,
+        gl_context,
+        window,
+    };
 
-        let mut handle_keyboard = |input: KeyboardInput| {
-            let pressed = match input.state {
-                ElementState::Pressed => true,
-                ElementState::Released => false,
-            };
-            match input.virtual_keycode {
-                Some(VirtualKeyCode::A) => {
-                    game.keyboard_input(Key::Left, pressed);
-                }
-                Some(VirtualKeyCode::S) => {
-                    game.keyboard_input(Key::Backward, pressed);
-                }
-                Some(VirtualKeyCode::D) => {
-                    game.keyboard_input(Key::Right, pressed);
-                }
-                Some(VirtualKeyCode::W) => {
-                    game.keyboard_input(Key::Forward, pressed);
-                }
-                Some(VirtualKeyCode::LShift) => {
-                    game.keyboard_input(Key::Down, pressed);
-                }
-                Some(VirtualKeyCode::Space) => {
-                    game.keyboard_input(Key::Up, pressed);
-                }
-                Some(VirtualKeyCode::Escape) => {
-                    control_flow.set_exit();
-                }
-                Some(VirtualKeyCode::F) => {
-                    window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
-                    //window.set_cursor_visible(false);
-                }
-                Some(VirtualKeyCode::G) => {
-                    //window.set_cursor_grab(CursorGrabMode::None).unwrap();
-                    //window.set_cursor_visible(true);
-                    window.set_fullscreen(None)
-                }
-                _ => {}
-            }
-        };
+    event_loop.run_app(&mut app).unwrap();
 
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::Resized(size) => {
-                    if size.width != 0 && size.height != 0 {
-                        surface.resize(
-                            &gl_context,
-                            NonZeroU32::new(size.width).unwrap(),
-                            NonZeroU32::new(size.height).unwrap(),
-                        );
-                        game.resize(glt, size);
-                    }
-                }
-                WindowEvent::Focused(is_focused) => {
-                    if is_focused {
-                        window.set_cursor_visible(false);
-                        //window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
-                    } else {
-                        //window.set_fullscreen(None);
-                        //window.set_cursor_grab(CursorGrabMode::None).unwrap();
-                        window.set_cursor_visible(true);
-                    }
-                }
-                WindowEvent::CloseRequested => {
-                    control_flow.set_exit();
-                }
-                WindowEvent::KeyboardInput {
-                    device_id: _,
-                    input,
-                    is_synthetic: _,
-                } => {
-                    handle_keyboard(input);
-                }
-                _ => (),
-            },
-            Event::MainEventsCleared => {
-                let current_time = std::time::SystemTime::now();
-                let delta_t = current_time.duration_since(now).unwrap();
-                now = current_time;
+    app.game.exit(app.glt);
 
-                println!("{}", delta_t.as_millis());
-
-                game.draw(glt, delta_t.as_secs_f32());
-
-                //game.print_dist();
-
-                surface.swap_buffers(&gl_context).unwrap();
-            }
-            Event::DeviceEvent {
-                device_id: _,
-                event,
-            } => match event {
-                DeviceEvent::MouseMotion { delta } => {
-                    game.mouse_input(delta);
-                }
-                _ => (),
-            },
-            Event::LoopDestroyed => {
-                println!("Loop destroyed");
-                //This is the last event so we can safely drop the game
-                unsafe {
-                    ManuallyDrop::take(&mut game).exit(glt);
-                }
-                //close interval server if it was started
-                if let Some(ref mut server_process) = &mut server_process {
-                    let mut stdin = server_process.stdin.take().unwrap();
-                    stdin.write_all(b"exit\n").unwrap();
-                    stdin.flush().unwrap();
-                    server_process.wait().unwrap();
-                }
-            }
-            _ => (),
-        }
-    });
+    //close interval server if it was started
+    if let Some(ref mut server_process) = &mut server_process {
+        let mut stdin = server_process.stdin.take().unwrap();
+        stdin.write_all(b"exit\n").unwrap();
+        stdin.flush().unwrap();
+        server_process.wait().unwrap();
+    }
 }
